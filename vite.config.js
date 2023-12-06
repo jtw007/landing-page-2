2ad04447edb615e860d33a16c5214810e378ac97
jtw007 <joshwu095@gmail.com>
2023-12-05 17:16:29 -0800

    set up tailwind, configured server port to 3000

--- vite.config.js
+++ vite.config.js
@@ -4,4 +4,8 @@ import react from '@vitejs/plugin-react-swc'
 // https://vitejs.dev/config/
 export default defineConfig({
   plugins: [react()],
+  server: {
+    // sets default port to localhost:3000 
+    port: 3000,
+  }
 })
